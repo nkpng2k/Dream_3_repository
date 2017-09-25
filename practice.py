0d31e792efd0676b9a63c691097683681f6e4c2f
Nick Png <nkpng2k@gmail.com>
2017-09-25 16:35:17 -0700

    committing some work on practice.py

--- practice.py
+++ practice.py
@@ -5,6 +5,7 @@ I will use these libraries in tandem with some classifiers to identify cards dea
 and count each card
 """
 from skimage import data, io, filters
+import numpy as np
 
 image = data.coins()
 edges = filters.sobel(image)
@@ -17,3 +18,15 @@ train a model to classify cards
 pass the model new image frames with a new card
 have the model identify it and count
 """
+# %cd /Users/npng/galvanize/Dream_3_repository
+
+#read in sample with grey scale -- Numpy values 0 = black, 1 = white
+card_image = io.imread('samples/IMG_1197.JPG', as_grey = True)
+
+# filters.thresholding.threshold_minimum finds minimum value to separate edges
+edges = filters.thresholding.threshold_minimum(card_image)
+filtered = card_image.copy()
+filtered[filtered < edges] = 0
+
+io.imshow(filtered)
+io.show()
