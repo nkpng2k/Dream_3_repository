6324e399fa48889dfae57493a8d94a5bc7124ed5
Nick Png <nkpng2k@gmail.com>
2017-09-26 21:26:13 -0700

    adding note

--- practice.py
+++ practice.py
@@ -4,46 +4,44 @@ The purpose of this is to learn how to operate scikit-image and scikit-video
 I will use these libraries in tandem with some classifiers to identify cards dealt
 and count each card
 """
-from skimage import data, io, filters, morphology, segmentation, measure, color
+import skimage as ski
 import numpy as np
 import matplotlib.pyplot as plt
+import PIL as pil
 
-image = data.coins()
-edges = filters.sobel(image)
-io.imshow(edges)
-io.show()
+image = ski.data.coins()
+edges = ski.filters.sobel(image)
+ski.io.imshow(edges)
+ski.io.show()
 
 """
 train a model to classify cards
 pass the model new image frames with a new card
 have the model identify it and count
 """
-# %cd /Users/npng/galvanize/Dream_3_repository
+%cd /Users/npng/galvanize/Dream_3_repository
 
 #read in sample with grey scale -- Numpy values 0 = black, 1 = white
-card_image = io.imread('samples/IMG_1197.JPG', as_grey = True)
+card_image = ski.io.imread('samples/IMG_1197.JPG', as_grey = True)
 
 # filters.thresholding.threshold_minimum finds minimum value to separate edges
-edges = filters.thresholding.threshold_minimum(card_image)
+edges = ski.filters.thresholding.threshold_minimum(card_image)
 filtered = card_image.copy()
 filtered[filtered < edges] = 0
 
-io.imshow(filtered)
-io.show()
+ski.io.imshow(filtered)
+ski.io.show()
 
-dbl_card = io.imread('samples/IMG_1199.jpg', as_grey = True)
+dbl_card = ski.io.imread('samples/IMG_1199.jpg', as_grey = True)
 
-edges = filters.thresholding.threshold_minimum(dbl_card)
+edges = ski.filters.thresholding.threshold_otsu(dbl_card)
 dbl_filtered = dbl_card.copy()
 dbl_filtered[dbl_filtered < edges] = 0
 
-io.imshow(dbl_filtered)
-io.show()
+ski.io.imshow(dbl_filtered)
+ski.io.show()
 
-
-
-
-
+#NOTE: need to use contour lines to generate bounding box
 
 
 """
