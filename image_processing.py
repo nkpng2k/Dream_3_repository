1e0c6be7e289c4e8a64e58feb86f8c75e5168291
Nick Png <nkpng2k@gmail.com>
2017-09-29 12:49:57 -0700

    sdlfjk

--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import glob
+
 
 class CardImageProcessing(object):
     """
@@ -10,6 +12,39 @@ class CardImageProcessing(object):
     OUTPUT: images processed into 1-D vectors as list of numpy arrays
     """
 
-    def __init__(self):
-        pass
-    
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+
+
+
+
+
+import glob
+images = [str(file) for file in glob.glob("/Users/npng/galvanize/Dream_3_repository/card_images/*")]
+images
+
+from os import listdir
+from os.path import isfile, join, splitext
+import numpy
+import cv2
+
+mypath='/Users/npng/galvanize/Dream_3_repository/card_images'
+onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+
+images = numpy.empty(len(onlyfiles), dtype=object)
+for n in range(0, len(onlyfiles)):
+  images[n] = cv2.imread( join(mypath,onlyfiles[n]) )
+
+for f in listdir(mypath):
+    fname, extension = splitext(f)
+    print fname, extension
+
+
+
+
+
+
+"""
+bottom of page
+"""
